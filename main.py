7934b8d322ff095f301993843d3fba62f0dec03c
Pranav Kethireddy <pran.kethi@gmail.com>
2023-02-18 17:38:42 -0600

    base of project

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pickle import APPEND
 from flask import request, Flask, render_template
 import requests
 import json
@@ -15,10 +16,22 @@ def home():
     elif request.method == 'POST':
         city = request.form['city']
         response = requests.get(f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{city}/today?unitGroup=metric&key=R7AJKMMK92F92AU32GWEFQ82M&contentType=json")
+        response1 = requests.get(f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{city}/next7days?unitGroup=metric&include=days&key=R7AJKMMK92F92AU32GWEFQ82M&contentType=json")
         report = response.json()
+        report1 = response1.json()
+        print(report1)
+        next_7_days = []
+        at = []
+        for days in report1['days']:
+            next_7_days.append(days['conditions'])
+            tma = days['tempmax']
+            tmi = days['tempmin']
+            at.append((tma+tmi)/2)
+            print(days['conditions'])
+            print(at)
+            
 
         print(response.status_code)
-        # print(response.json())
 
         conditions = []
         ra = report['resolvedAddress'] 
@@ -38,8 +51,8 @@ def home():
                 ws = hours['windspeed']   
                 cc = hours['cloudcover'] 
                 se = hours['solarenergy']
-                conditions.append(f"{icon}, {temp} , {hum}, {perci}, {ws}, {cc}, {se}")         
-        return render_template('home.html', conditions=conditions, daily=daily)
+                conditions.append(f" {icon}, {temp} , {hum}, {perci}, {ws}, {cc}, {se}")         
+        return render_template('home.html', conditions=conditions, daily=daily, next_7_days=next_7_days, at=at)
 
 
 if __name__ == '__main__':
